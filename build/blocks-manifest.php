32355873d738272a4224eb6e0c93ec1f9832f83f
robertdev18 <robertdev18@gmail.com>
2025-04-08 22:22:27 -0400

    starting

---
+++ build/blocks-manifest.php
@@ -0,0 +1,26 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'furryblock' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'wpdev/furryblock',
+		'version' => '0.1.0',
+		'title' => 'Furryblock',
+		'category' => 'widgets',
+		'icon' => 'smiley',
+		'description' => 'Example block scaffolded with Create Block tool.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'furryblock',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'render' => 'file:./render.php',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ build/furryblock/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'da20d940a10eb7da9e72');
